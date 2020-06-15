624ed52fa91a76ea8b86e3774d8d00f39cc27619
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2020-06-15 16:39:38 +0800

    Add notification instance cleaner sql queries

--- groovy/users/deleteInActiveUsersFromRoles.groovy
+++ groovy/users/deleteInActiveUsersFromRoles.groovy
@@ -1,20 +1,15 @@
 boolean isPreview = true
 // remove inactive users from project roles
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.ComponentManager
+import com.atlassian.jira.component.pico.ComponentManager
 import com.atlassian.jira.bc.user.search.UserSearchService
 import com.atlassian.jira.bc.user.search.UserSearchParams
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.jira.util.SimpleErrorCollection
 import com.atlassian.jira.bc.projectroles.ProjectRoleService
 import com.atlassian.jira.project.Project
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
 
-
-def log = Logger.getLogger("com.gonchik.scripts.groovy.deleteInActiveUsersFromGroupAndRoles")
-log.setLevel(Level.DEBUG)
-
+def sb = new StringBuilder()
 
 UserSearchService userSearchService = ComponentAccessor.getComponent(UserSearchService.class)
 UserSearchParams userSearchParams = (new UserSearchParams.Builder()).allowEmptyQuery(true).includeActive(false).includeInactive(true).maxResults(100000).build()
@@ -22,13 +17,14 @@ for (ApplicationUser appUser : userSearchService.findUsers("", userSearchParams)
     ApplicationUser user = appUser
     def userUtil = ComponentAccessor.userUtil
     // Remove user from all roles...
-    ProjectRoleService projectRoleService = (ProjectRoleService) ComponentManager.getComponentInstanceOfType(ProjectRoleService.class)
+    ProjectRoleService projectRoleService = ComponentAccessor.getComponent(ProjectRoleService.class)
     SimpleErrorCollection errorCollection = new SimpleErrorCollection()
     projectRoleService.getProjectsContainingRoleActorByNameAndType(user.getName(), 'atlassian-user-role-actor', errorCollection).each { Project project ->
-        log.debug("Remove user ${user.getName()} from roles reference: ${project.getName()}")
+        def line = "Remove user ${user.getName()} from roles reference: ${project.getName()} <br>"
+        sb.append(line)
         if (!isPreview) {
             projectRoleService.removeAllRoleActorsByNameAndType(user.getName(), 'atlassian-user-role-actor', errorCollection)
         }
     }
-    println errorCollection.dump()
 }
+return sb.toString()
---
+++ sql/jria_notification_instance_cleaner.sql
@@ -0,0 +1,49 @@
+/* Count number of email references */
+select count(ID)
+FROM notificationinstance;
+
+SET SQL_SAFE_UPDATES=0;
+/* Delete unassociated messageID without references to tickets */
+DELETE
+FROM notificationinstance
+where SOURCE not in (select ID from jiraissue);
+
+/* Delete references to issues with resolution and NOT updated last 12 week */
+DELETE
+FROM notificationinstance
+where SOURCE IN (select ID
+					from jiraissue
+                    where RESOLUTION is not null and
+						UPDATED < (NOW() - INTERVAL 12 WEEK)
+					);
+
+/* Delete references to closed issues  and NOT updated last 4 week */
+DELETE
+FROM notificationinstance
+where SOURCE IN ( select ID
+				  FROM jiraissue
+				  where issuestatus in (SELECT id
+										FROM issuestatus
+										where pname in ('Closed', 'Done') )
+						AND UPDATED < (NOW() - INTERVAL 4 WEEK));
+
+/* Remove empty MESSAGEID rows */
+DELETE FROM notificationinstance
+where messageid like '';
+
+
+/* Check duplicates of MessageID */
+SELECT SOURCE, MESSAGEID, COUNT(MESSAGEID)
+FROM notificationinstance
+GROUP BY MESSAGEID
+HAVING COUNT(MESSAGEID) > 1
+LIMIT 0, 10;
+
+
+
+/* Remove duplicated MESSAGEID rows, keep lowest id */
+delete n1
+FROM notificationinstance n1,
+notificationinstance n2
+WHERE n1.id > n2.id
+AND n1.MESSAGEID = n2.MESSAGEID;
