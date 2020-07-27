810b3c51c175415aa62cbdae67ac02035758c707
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2020-07-27 18:08:21 +0300

    Clean attachments better and faster

--- groovy/confluence/attachmentCleaner.groovy
+++ groovy/confluence/attachmentCleaner.groovy
@@ -5,31 +5,43 @@ import com.atlassian.confluence.pages.PageManager
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import java.util.ConcurrentModificationException
+import com.atlassian.confluence.pages.Page
+import com.atlassian.confluence.core.ContentEntityObject
 
 
-def log = Logger.getLogger("com.gonchik.scripts.groovy.confluence.attachemntCleaner")
+def log = Logger.getLogger("com.gonchik.scripts.groovy.confluence.attachmentCleaner")
 log.setLevel(Level.DEBUG)
 final PageManager pageManager = ComponentLocator.getComponent(PageManager)
-final SpaceManager spaceManager = ComponentLocator.getComponent(SpaceManager.class)
+final SpaceManager spaceManager = ComponentLocator.getComponent(SpaceManager)
+final AttachmentManager attachmentManager = ComponentLocator.getComponent(AttachmentManager)
 
-spaceManager.getAllSpaces().each { space ->
-    pageManager.getPages(space, true).each { page ->
-        long pageId = page.id
-        try {
-            cleanPreviousVerionsFromContent(pageId)
-        } catch (ConcurrentModificationException e) {
-            log.error("Please, wait then do rerun")
-        }
+boolean allSpaces = false
+def spaceKey = "CONDUCTOR50"
+def spaces = []
+
+if (allSpaces) {
+    spaces = spaceManager.getAllSpaces()
+} else {
+    spaces = spaceManager.getSpace(spaceKey)
+}
+
+for (def space : spaces) {
+    log.debug "Review " + space.name
+    def pages = pageManager.getPages(space, true)
+    for (Page page : pages) {
+        log.debug "Review page " + page.getNameForComparison()
+        cleanAttachedPreviousVersionsFromContent(page)
+        log.debug "End review pages"
     }
 }
 
 
-def cleanPreviousVerionsFromContent(long pageId) throws ConcurrentModificationException {
-    def attachmentManager = ComponentLocator.getComponent(AttachmentManager.class)
-    def pageManager = ComponentLocator.getComponent(PageManager)
-    def page = pageManager.getPage(pageId)
-    def attachments = page.getAttachments()
-
+private void cleanAttachedPreviousVersionsFromContent(Page page) {
+    def attachments = attachmentManager.getLatestVersionsOfAttachments(page)
+    if (attachments == null || attachments.size == 0) {
+        log.debug "Empty attachments"
+        return
+    }
     for (def attachment : attachments) {
         attachmentManager.getPreviousVersions(attachment).each { it ->
             log.debug("Removing " + it.getFileName() + "")
