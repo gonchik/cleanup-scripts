027e5ff2311a51676cae630bee97ccd8ec5b412f
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2020-08-25 15:03:09 +0300

    Polish queries

--- sql/jira/jira_rudiment_detector.sql
+++ sql/jira/jira_rudiment_detector.sql
@@ -1,12 +1,12 @@
-SELECT count(id) from entity_property where ENTITY_NAME = 'fusion.caches.issue';
-SELECT count(id) from entity_property where ENTITY_NAME = 'hipchat.integration.caches.issue-mentions';
+SELECT count(id) FROM entity_property WHERE ENTITY_NAME = 'fusion.caches.issue';
+SELECT count(id) FROM entity_property WHERE ENTITY_NAME = 'hipchat.integration.caches.issue-mentions';
 
 
 
 -- DETECT HOW MANY VALUES DO YOU HAVE IN Development panel
 -- FYI: that info is like SQL cache
-select count(ID) from AO_575BF5_PROVIDER_ISSUE;
-select count(ID) from AO_575BF5_DEV_SUMMARY;
+SELECT count(ID) FROM AO_575BF5_PROVIDER_ISSUE;
+SELECT count(ID) FROM AO_575BF5_DEV_SUMMARY;
 
 -- truncate AO_575BF5_DEV_SUMMARY;
 -- truncate AO_575BF5_PROVIDER_ISSUE;
