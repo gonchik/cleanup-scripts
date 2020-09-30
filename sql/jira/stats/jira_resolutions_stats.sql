b319d3c113ef7cb5c538d4817af8c6a89ad46738
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2020-09-30 14:53:33 +0300

    Jira: stats of resolutions

---
+++ sql/jira/stats/jira_resolutions_stats.sql
@@ -0,0 +1,7 @@
+-- How many tickets linked to exact Resolutions in Jira
+SELECT re.pname,
+		(SELECT count(ji.id)
+         FROM jiraissue ji
+         WHERE re.id = ji.RESOLUTION) as "Number of linked tickets"
+FROM resolution re
+ORDER BY 2 ASC;
