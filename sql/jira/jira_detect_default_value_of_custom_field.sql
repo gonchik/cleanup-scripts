221d410dce00ae759155390d40d9b7a57c6dc942
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2020-12-24 12:46:52 +0300

    Jira: add sql script to detect default values of custom fields

---
+++ sql/jira/jira_detect_default_value_of_custom_field.sql
@@ -0,0 +1,6 @@
+-- That query helps to understand the default value of custom field
+-- do cleanup after that
+SELECT customfield.cfname, concat('customfield_' , customfield.ID)
+FROM fieldconfigscheme
+INNER JOIN genericconfiguration on genericconfiguration.DATAKEY = fieldconfigscheme.ID
+JOIN customfield on concat('customfield_' , customfield.ID) = fieldconfigscheme.FIELDID;
