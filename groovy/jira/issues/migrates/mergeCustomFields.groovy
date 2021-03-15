832862ecf9e633851e066c7b3c70c166441b3dd0
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2021-03-15 19:29:51 +0300

    Jira: Add merge field script

---
+++ groovy/jira/issues/migrates/mergeCustomFields.groovy
@@ -0,0 +1,68 @@
+/*
+*  That script just migrate and merge 2 custom fields with the same types
+*   This script works without notification and as a service
+*
+* */
+
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.CustomFieldManager
+import com.atlassian.jira.issue.fields.CustomField
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.MutableIssue
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import com.atlassian.jira.event.type.*
+import com.atlassian.jira.util.ImportUtils
+import com.atlassian.jira.issue.ModifiedValue
+import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
+import com.atlassian.jira.issue.index.IssueIndexingService
+import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.bc.issue.search.SearchService.ParseResult
+import com.atlassian.jira.web.bean.PagerFilter
+
+def log = Logger.getLogger("com.gonchik.scripts.groovy.migrate.values")
+log.setLevel(Level.DEBUG)
+
+String jqlSearch = '"External issue ID 1" is not EMPTY'
+boolean cleanSourceField = false
+
+CustomFieldManager fieldManager = ComponentAccessor.getCustomFieldManager()
+CustomField sourceCustomField = fieldManager.getCustomFieldObjectByName("External issue ID 1")
+CustomField destinationCustomField = fieldManager.getCustomFieldObjectByName("External issue ID")
+def issueIndexingService = ComponentAccessor.getComponent(IssueIndexingService.class)
+
+def user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+IssueManager issueManager = ComponentAccessor.getIssueManager()
+SearchService searchService = ComponentAccessor.getComponent(SearchService.class)
+SearchService.ParseResult parseResult = searchService.parseQuery(user, jqlSearch)
+if (!parseResult.isValid()) {
+    log.debug("JQL is not correct")
+    return 1
+}
+
+def searchResult = searchService.search(user, parseResult.getQuery(), PagerFilter.getUnlimitedFilter())
+def issues = searchResult.results.collect { issueManager.getIssueObject(it.id) }
+
+for (issue in issues) {
+    log.debug("Before change is " + issue.getCustomFieldValue(destinationCustomField))
+    //Transition issue by transition name and not by transition id, very useful for queries with issues with different workflows
+    def value = sourceCustomField.getValue(issue)
+    MutableIssue issueToUpdate = (MutableIssue) issue
+    destinationCustomField.updateValue(null, issueToUpdate, new ModifiedValue("", value), new DefaultIssueChangeHolder())
+    // updates with history
+    // issueToUpdate.setCustomFieldValue(destinationCustomField, value)
+    // issueManager.updateIssue(user, issueToUpdate, EventDispatchOption.DO_NOT_DISPATCH, false)
+    if (cleanSourceField) {
+        sourceCustomField.updateValue(null, issueToUpdate, new ModifiedValue(null, null), new DefaultIssueChangeHolder())
+    }
+
+    boolean wasIndexing = ImportUtils.isIndexIssues()
+    ImportUtils.setIndexIssues(true)
+    log.warn("Reindex issue ${issue.key} ${issue.id}")
+    issueIndexingService.reIndex(issue)
+    ImportUtils.setIndexIssues(wasIndexing)
+    log.debug("After change is " + issue.getCustomFieldValue(destinationCustomField))
+}
+
+
