ac6593ce40accdb51878d80825f4cfdefad9e574
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2021-03-19 04:21:43 +0300

    Add sq; query related to the AUDIT logs

--- sql/jira/jira_service_desk_sla_audit_checker.sql
+++ sql/jira/jira_service_desk_sla_audit_checker.sql
@@ -1,24 +1,69 @@
 -- If you have a huge number of customisation
 -- with scripts, SQL scripts to SLA you can meet with inconsistency
 
-SELECT SLA_AUDIT_LOG_ID
-FROM AO_54307E_SLAAUDITLOGDATA
-HAVING COUNT(SLA_AUDIT_LOG_ID) > 1;
 
-SELECT *
+-- let's check consistency 2 tables
+SELECT count(*)
+FROM AO_54307E_SLAAUDITLOG
+where "ID" not in (SELECT "SLA_AUDIT_LOG_ID"
+                    FROM "AO_54307E_SLAAUDITLOGDATA");
+
+
+SELECT count(*)
+FROM "AO_54307E_SLAAUDITLOG"  AL
+LEFT JOIN "AO_54307E_SLAAUDITLOGDATA" AD
+        ON AD."SLA_AUDIT_LOG_ID" = AL."ID"
+WHERE AD."SLA_AUDIT_LOG_ID" is null;
+
+
+
+SELECT count(*)
 FROM "AO_54307E_SLAAUDITLOGDATA"
-WHERE "SLA_AUDIT_LOG_ID" IN (SELECT "SLA_AUDIT_LOG_ID"
-			   FROM "AO_54307E_SLAAUDITLOGDATA"
-			   GROUP BY "SLA_AUDIT_LOG_ID"
-			   HAVING COUNT("SLA_AUDIT_LOG_ID") > 1);
+WHERE "SLA_AUDIT_LOG_ID" NOT IN (SELECT "ID"
+			                    FROM "AO_54307E_SLAAUDITLOG")
+			                    ;
 
 
 SELECT count("SLA_AUDIT_LOG_ID")
-FROM "AO_54307E_SLAAUDITLOGDATA";
+FROM "AO_54307E_SLAAUDITLOGDATA" AD
+LEFT JOIN "AO_54307E_SLAAUDITLOG" AL
+    ON AD."SLA_AUDIT_LOG_ID" = AL."ID"
+WHERE AL."ID" is null;
+
+
+
+
+
+-- Let's cleanup old audit logs
+SELECT count(*)
+FROM "AO_54307E_SLAAUDITLOG"
+WHERE to_timestamp("EVENT_TIME"/1000) < NOW() - INTERVAL '90 days';
+
+
+
+
+
+-- detect orphaned SLA Audit log
+SELECT count("SLA_AUDIT_LOG_ID")
+FROM "AO_54307E_SLAAUDITLOGDATA"
+WHERE "SLA_AUDIT_LOG_ID" in (SELECT "ID"
+                             FROM "AO_54307E_SLAAUDITLOG"
+                             WHERE "ISSUE_ID" is null);
 
 SELECT count(*)
-FROM "AO_54307E_SLAAUDITLOG";
+FROM "AO_54307E_SLAAUDITLOG"
+WHERE "ISSUE_ID" is null;
 
 
-SELECT to_timestamp(EVENT_TIME)
-FROM "AO_54307E_SLAAUDITLOG";
+/*
+DELETE
+FROM "AO_54307E_SLAAUDITLOGDATA"
+WHERE "SLA_AUDIT_LOG_ID" in (SELECT "ID"
+                             FROM "AO_54307E_SLAAUDITLOG"
+                             WHERE "ISSUE_ID" is null);
+
+DELETE
+FROM "AO_54307E_SLAAUDITLOG"
+WHERE "ISSUE_ID" is null;
+*/
+
