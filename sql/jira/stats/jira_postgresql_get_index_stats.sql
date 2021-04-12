0479fe60e7b027e554789f422dfa710e378a1364
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2021-04-12 12:00:32 +0300

    Get index info

---
+++ sql/jira/stats/jira_postgresql_get_index_stats.sql
@@ -0,0 +1,25 @@
+/*
+ *
+    Get stats of your DB indexes.
+    Interesting to understand which indexes is not used on your instance.
+    After you can easier review tables via \dt command
+*/
+
+SELECT s.schemaname,
+       s.relname AS tablename,
+       s.indexrelname AS indexname,
+       pg_size_pretty(pg_relation_size(s.indexrelid::regclass)) AS index_size,
+       idx_tup_read,
+       idx_tup_fetch,
+       idx_scan
+FROM pg_catalog.pg_stat_user_indexes s
+   JOIN pg_catalog.pg_index i ON s.indexrelid = i.indexrelid
+WHERE
+  s.idx_scan = 0      					-- has never been scanned
+  AND 0 <>ALL (i.indkey)  				-- no index column is an expression
+  AND NOT i.indisunique  				-- is not a UNIQUE index
+  AND NOT EXISTS          				-- does not enforce a constraint
+         (SELECT 1 FROM pg_catalog.pg_constraint c
+          WHERE c.conindid = s.indexrelid)
+ORDER BY pg_relation_size(s.indexrelid) DESC
+LIMIT 30;
