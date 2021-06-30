f831d58cf86805ab91ec55867a098ee1571c9066
Gonchik Tsymzhitov <gonchik.tsymzhitov@gmail.com>
2021-07-01 01:53:47 +0300

    Polish

--- sql/jira/jira_insight_history_object_stats.sql
+++ sql/jira/jira_insight_history_object_stats.sql
@@ -1,6 +1,22 @@
 -- Get Insight history attributes for the retention policy
 
-SELECT hist."AFFECTED_ATTRIBUTE", count(hist."ID")  updates
+SELECT hist."AFFECTED_ATTRIBUTE",
+       count(hist."ID")  updates
 FROM "AO_8542F1_IFJ_OBJ_HIST" hist
 GROUP BY hist."AFFECTED_ATTRIBUTE"
+ORDER BY updates DESC;
+
+-- only ID aggregated
+SELECT hist."ID",
+       count(hist."ID")  updates
+FROM "AO_8542F1_IFJ_OBJ_HIST" hist
+GROUP BY hist."ID"
+ORDER BY updates DESC;
+
+-- aggregated for both for small asset management
+SELECT hist."ID",
+       hist."AFFECTED_ATTRIBUTE",
+       count(hist."ID")  updates
+FROM "AO_8542F1_IFJ_OBJ_HIST" hist
+GROUP BY hist."ID", hist."AFFECTED_ATTRIBUTE"
 ORDER BY updates DESC;
