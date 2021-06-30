43ab22ba0dd659d43323abbd5e081a42318a6ede
Gonchik Tsymzhitov <gonchik.tsymzhitov@gmail.com>
2021-07-01 02:06:09 +0300

    Some clarifications

--- sql/jira/jira_insight_history_object_stats.sql
+++ sql/jira/jira_insight_history_object_stats.sql
@@ -6,17 +6,19 @@ FROM "AO_8542F1_IFJ_OBJ_HIST" hist
 GROUP BY hist."AFFECTED_ATTRIBUTE"
 ORDER BY updates DESC;
 
+
+-- the next query is extra
 -- only ID aggregated
-SELECT hist."ID",
+SELECT hist."OBJECT_TYPE_ATTRIBUTE_ID",
        count(hist."ID")  updates
 FROM "AO_8542F1_IFJ_OBJ_HIST" hist
-GROUP BY hist."ID"
+GROUP BY hist."OBJECT_TYPE_ATTRIBUTE_ID"
 ORDER BY updates DESC;
 
 -- aggregated for both for small asset management
-SELECT hist."ID",
+SELECT hist."OBJECT_TYPE_ATTRIBUTE_ID",
        hist."AFFECTED_ATTRIBUTE",
        count(hist."ID")  updates
 FROM "AO_8542F1_IFJ_OBJ_HIST" hist
-GROUP BY hist."ID", hist."AFFECTED_ATTRIBUTE"
+GROUP BY hist."OBJECT_TYPE_ATTRIBUTE_ID", hist."AFFECTED_ATTRIBUTE"
 ORDER BY updates DESC;
