9761fbf0c60909964e60e3dedfcce9bacb6ec11f
Gonchik Tsymzhitov <gonchik.tsymzhitov@atlassiancommunity.com>
2024-01-21 23:42:34 +0200

    Add additional function

--- sh/os_cleanup.sh
+++ sh/os_cleanup.sh
@@ -238,3 +238,31 @@ posgresql_cleanup() {
   echo 'Truncate logs of PostgreSQL'
   find /var/lib/pgsql/11/data/pg_log/ -name "*.log"  -exec truncate {} --size 0 \;
 }
+
+
+android_sdk_cleanup_for_mac() {
+  # Deletes the Android Studio application
+  # Note that this may be different depending on what you named the application as, or whether you downloaded the preview version
+  rm -Rf /Applications/Android\ Studio.app
+  # Delete All Android Studio related preferences
+  # The asterisk here should target all folders/files beginning with the string before it
+  rm -Rf ~/Library/Preferences/AndroidStudio*
+  rm -Rf ~/Library/Preferences/Google/AndroidStudio*
+  # Deletes the Android Studio's plist file
+  rm -Rf ~/Library/Preferences/com.google.android.*
+  # Deletes the Android Emulator's plist file
+  rm -Rf ~/Library/Preferences/com.android.*
+  # Deletes mainly plugins (or at least according to what mine (Edric) contains)
+  rm -Rf ~/Library/Application\ Support/AndroidStudio*
+  rm -Rf ~/Library/Application\ Support/Google/AndroidStudio*
+  # Deletes all logs that Android Studio outputs
+  rm -Rf ~/Library/Logs/AndroidStudio*
+  rm -Rf ~/Library/Logs/Google/AndroidStudio*
+  # Deletes Android Studio's caches
+  rm -Rf ~/Library/Caches/AndroidStudio*
+  rm -Rf ~/Library/Caches/Google/AndroidStudio*
+  # Deletes older versions of Android Studio
+  rm -Rf ~/.AndroidStudio*
+  rm -Rf ~/.gradle
+  rm -Rf /usr/local/var/lib/android-sdk/
+}
