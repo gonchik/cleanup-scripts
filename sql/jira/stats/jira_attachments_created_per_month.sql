f61c368b1c29aec6929de8849f59c1e387d0ec24
Gonchik Tsymzhitov <gonchik.tsymzhitov@atlassiancommunity.com>
2024-08-21 11:37:46 +0300

    Add more stats info

--- sql/jira/stats/jira_attachments_created_per_month.sql
+++ sql/jira/stats/jira_attachments_created_per_month.sql
@@ -3,31 +3,37 @@
 
 */
 
-SELECT to_char(created, 'YYYY-MM') AS "Year",
-       COUNT(*)                    AS "Number of Attachments"
-FROM fileattachment
-GROUP BY to_char(created, 'YYYY-MM')
-ORDER BY 1 LIMIT 100;
-
+-- check per year created files
 SELECT to_char(created, 'YYYY') AS "Year",
-       COUNT(*)                    AS "Number of Attachments"
-FROM fileattachment
+       COUNT(*)                 AS "Number of Attachments"
+FROM fileattachment fa
 GROUP BY to_char(created, 'YYYY')
 ORDER BY 1 LIMIT 100;
 
+-- check per month
+SELECT to_char(created, 'YYYY-MM') AS "Year",
+       COUNT(*)                    AS "Number of Attachments"
+FROM fileattachment fa
+GROUP BY to_char(created, 'YYYY-MM')
+ORDER BY 1 LIMIT 100;
+
+
+/*
+    Bottom query for MySQL
+ */
 
 -- mysql query for monthly
 SELECT DATE_FORMAT(created, '%Y-%m') as "Months",
        COUNT(*)                      as "Number of Attachments"
-FROM fileattachment
+FROM fileattachment fa
 GROUP BY YEAR (created), MONTH (created)
 ORDER BY 1
     LIMIT 100;
 
 -- mysql query for yearly
-SELECT DATE_FORMAT(created, '%Y') as "Months",
-       COUNT(*)                      as "Number of Attachments"
-FROM fileattachment
+SELECT DATE_FORMAT(created, '%Y') as "Year",
+       COUNT(*)                   as "Number of Attachments"
+FROM fileattachment fa
 GROUP BY YEAR (created), MONTH (created)
 ORDER BY 1
     LIMIT 100;
