1296f29f96e47c87958ad96a1ee1c35fe5ddc28a
Gonchik Tsymzhitov <gonchik.tsymzhitov@gmail.com>
2025-05-01 01:24:31 +0300

    Update upgrade_jira.sh

--- sh/upgrade_jira.sh
+++ sh/upgrade_jira.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Main variables
-NEW_RELEASE=atlassian-jira-software-9.4.22-standalone
-OLD_RELEASE=atlassian-jira-software-9.4.15-standalone
+NEW_RELEASE=atlassian-jira-software-9.17.5-standalone
+OLD_RELEASE=atlassian-jira-software-9.4.22-standalone
 APP_USER=jira
 APP_HOME=/var/atlassian/application-data/jira
 APP_INSTALL_DIR=/opt/atlassian/jira
 
 cd ${APP_INSTALL_DIR} || exit
 
-wget -c https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-9.4.22.tar.gz
-tar -xzvf atlassian-jira-software-9.4.22.tar.gz
+wget -c https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-9.17.5.tar.gz
+tar -xzvf atlassian-jira-software-9.17.5.tar.gz
 
 
 
@@ -38,7 +38,7 @@ echo "Custom mail logo"
 cp {${OLD_RELEASE},${NEW_RELEASE}}/atlassian-jira/images/mail/logo.png
 
 echo "Custom language pack"
-yes | cp {${OLD_RELEASE},${NEW_RELEASE}}/atlassian-jira/WEB-INF/atlassian-bundled-plugins/jira-core-language-pack-ru_RU-9.8.0.v20230228123141.jar
+yes | cp {${OLD_RELEASE},${NEW_RELEASE}}/atlassian-jira/WEB-INF/atlassian-bundled-plugins/jira-core-language-pack-ru_RU-10.0.0.v20240619070432.jar
 echo "Removed cached backup files"
 rm -f ${APP_HOME}/customisations-backup/atlassian-jira/WEB-INF/atlassian-bundled-plugins/jira-core-language-pack-ru_RU-9.8.0.v20230228123141.jar
 rm -f ${APP_HOME}/customisations/atlassian-jira/WEB-INF/atlassian-bundled-plugins/jira-core-language-pack-ru_RU-9.8.0.v20230228123141.jar
@@ -52,19 +52,20 @@ ln -s ${NEW_RELEASE} current
 echo "Cleaning plugin cache"
 rm -rf ${APP_HOME}/plugins/.bundled-plugins
 rm -rf ${APP_HOME}/plugins/.osgi-plugins
-rm -f ${APP_HOME}/.jira.home.lock
+rm -f  ${APP_HOME}/.jira.home.lock
 echo "Clean old logs from old installation"
-rm -f ${OLD_RELEASE}/logs/*
+rm -f  ${OLD_RELEASE}/logs/*
 rm -rf ${OLD_RELEASE}/temp/*
 
 
 # extra step if you have JSM and Software on the same installation
 # https://confluence.atlassian.com/adminjiraserver/upgrading-jira-data-center-with-zero-downtime-938846953.html
-#
+# https://marketplace.atlassian.com/apps/1213632/jira-service-desk/version-history?includeHiddenVersions=1
+# @todo make function
 # mkdir jsm
 # cd jsm
-# wget -c https://marketplace.atlassian.com/download/apps/1213632/version/1050040022
-# unzip 1050040022
+# wget -c https://marketplace.atlassian.com/download/apps/1213632/version/1050170005
+# unzip 1050170005
 #
 # cp *.jar dependencies/*.jar ${APP_HOME}/sharedhome/plugins/installed-plugins/
 # cd ..
